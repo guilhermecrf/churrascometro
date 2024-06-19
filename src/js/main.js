5488a0ee39bc7f732cdc345cf215b9de6aa88e24
Guilherme Santana <guilhermedrxs@gmail.com>
2024-06-19 14:46:05 -0300

    finished main.js

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,9 @@ let inputAdults = document.querySelector('#adults')
 let inputKids = document.querySelector('#kids')
 let inputTiming = document.querySelector('#timing')
 
+// -- prevent form default (e) and calculate 
+// -- and calculating how much barbecue you will need
+
 form.onsubmit = event => {
     event.preventDefault()
 
@@ -18,4 +21,37 @@ form.onsubmit = event => {
     let totalAmountDrinks = drinksPerPerson(timing) * adults + (drinksPerPerson(timing) / 2 * kids)
 
     displayResultMessage(totalAmountMeat, totalAmountBeer, totalAmountDrinks)
+}
+
+function meatPerPerson(timing) {
+    if(timing >= 6) {
+        return 650
+    }else {
+        return 400
+    }
+}
+
+function beerPerPerson(timing) {
+    if(timing >= 6) {
+        return 2000
+    }else {
+        return 1200
+    }
+}
+
+function drinksPerPerson(timing) {
+    if(timing >= 6) {
+        return 1500
+    }else {
+        return 1000
+    }
+}
+
+// -- modal pop-up --
+
+function displayResultMessage(totalAmountMeat, totalAmountBeer, totalAmountDrinks) {
+    const message = `Precisará de ${totalAmountMeat / 1000} kg de carne, ${Math.ceil(totalAmountBeer / 355)} latas de cerveja e, ${Math.ceil(totalAmountDrinks / 2000)} garrafas de bebidas.`
+
+    Modal.message.innerText = message
+    Modal.open()
 }
